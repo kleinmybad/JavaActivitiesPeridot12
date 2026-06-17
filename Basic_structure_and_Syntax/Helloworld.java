936a2d091333e4d4051c0f8510d0f3621b647bbd
kleinmybad <babydazer7@gmail.com>
2026-06-17 14:42:33 +0800

    june 17 2026

--- Basic_structure_and_Syntax/Helloworld.java
+++ Basic_structure_and_Syntax/Helloworld.java
@@ -6,8 +6,32 @@ public static void main(String []shaolin)
 {
  System.out.println("Hello,world");
  System.out.println("I'm John jhezter N. romangaya");
+ System.out.println("136635140136l");
  System.out.println("I'm 16 years old");
  System.out.println("I live in Caloocan");
  System.out.println("I am student at Grade 12 Peridot!");
+
+ System.out.println("addition"); // this is addition
+ System.out.print("6 + 7 = "); 
+ System.out.println(6 + 7);
+ 
+ System.out.println("Substraction"); // this is substraction
+ System.out.print("5 - 3 = ");
+ System.out.println(5 - 3);
+ 
+ System.out.println("Multiplication"); // this is multiplication
+ System.out.print ("10 * 5 = ");
+ System.out.println(10 * 5);
+
+ System.out.println("division"); // this is division
+ System.out.print("5 / 5 = ");
+ System.out.println(5 / 5); 
+
+
+
+
+
+
+
 }
 }
