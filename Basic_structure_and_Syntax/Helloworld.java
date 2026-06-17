32505aa8288d7b679494b143e5181d5285a9544e
kleinmybad <babydazer7@gmail.com>
2026-06-17 13:19:26 +0800

    myfirstcommit

--- Basic_structure_and_Syntax/Helloworld.java
+++ Basic_structure_and_Syntax/Helloworld.java
@@ -4,10 +4,10 @@ public class Helloworld {
 
 public static void main(String []shaolin)
 {
- System.out.println("Hello,nigger :)");
- System.out.println("I'm Pew");
- System.out.println("I'm 91 years old");
- System.out.println("I live in dark continent");
+ System.out.println("Hello,world");
+ System.out.println("I'm John jhezter N. romangaya");
+ System.out.println("I'm 16 years old");
+ System.out.println("I live in Caloocan");
  System.out.println("I am student at Grade 12 Peridot!");
 }
 }
